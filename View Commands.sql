8e383c04c699b2aabe1461f0718cdb4fa9e8495d
prashasti19075 <55681151+prashasti19075@users.noreply.github.com>
2021-06-13 16:22:05 +0530

    Update View Commands.sql

--- View Commands.sql
+++ View Commands.sql
@@ -114,6 +114,43 @@ CREATE VIEW contestant_info AS
         submission
     WHERE
         CandidateNo = SUBSTRING_INDEX(USER(), '@', 1);
+-- CREATE VIEW PanelistView 
+-- AS 
+-- SELECT * FROM panellist, submission
+-- WHERE panellist.CandidateNo=convert(left(CURRENT_USER(),INSTR(current_user(), '@')-1), decimal);
+
+create user "30001"@localhost identified by "12345";
+
+select substring_index(user(), '@', 1);
+
+CREATE VIEW PanelistView 
+AS 
+SELECT * FROM panellist, submission
+WHERE panellist.PanellistNo= substring_index(user(), '@', 1);
+drop view PanelistView;
+
+create view Contestant as 
+with short(num, CategorySelectedFor) as (select candidate.candidateNo, shortlisted.category from candidate left join shortlisted on candidate.candidateNo=shortlisted.candidateNo)
+select 
+S.CandidateNo, S.Duration, S.SubmissionDate, S.SubmissionType,  C.firstName, C.LastName, C.DOB, C.PriorExperience, C.Email, C.AdvertisementSeen, Short.CategorySelectedFor 
+from Candidate as C 
+inner join submission as S 
+on C.CandidateNo=S.CandidateNo 
+inner join short
+on S.CandidateNo=Short.num
+where S.CandidateNo=substring_index(user(), '@', 1);
+
+create view MemView
+as with temp(mgid) as 
+(select roles.musicgroupid from roles where roles.MemberNo=substring_index(user(), '@', 1)) 
+select trailer.likes, trailer.dislikes, trailer.DateOfRelease, trailer.NumberOfVisits, m.musicgroupid, m.moderatorno, m.type, r.Albumno, r.MemberNo, r.RolePlayed, album.Albumname, album.albumtype, album.approvalstatus, album.approvalDate, album.URL 
+from MusicGroup as m 
+inner join roles as r 
+on m.musicgroupid=r.musicgroupid and m.musicgroupid in (select mgid from temp) 
+inner join Album on r.AlbumNo=Album.AlbumNo
+inner join trailer on trailer.AlbumNo=Album.AlbumNo;
+
+grant select on PanelistView to "30001"@localhost;
 
 
 create role role_contestant;
